b541a5369c028400d9076d76d37d14dca848931f
Hector Toro <asdxdd7@gmail.com>
2025-04-12 14:29:42 -0400

    Datos añadidos al contenido

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -24,16 +24,27 @@ const LandingPage = () => {
   // Algunas secciones de contenido para que aparezcan en tandas.
   // Podrías obtener estos textos de un array y mapearlos, aquí lo hago manualmente a modo de ejemplo.
   const sections = [
-    `Introducción a su carrera. Ej: Sus inicios en el arte, su estilo
-    inconfundible y su impacto en la escena artística contemporánea.`,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet lectus
-    id nisl luctus accumsan. Donec feugiat lectus vel magna faucibus, ac aliquam
-    velit imperdiet.`,
-    `Sed dictum, sapien eget pretium hendrerit, nisl eros cursus elit, sed interdum
-    urna sem sed tortor. Curabitur bibendum leo vitae dui blandit, non placerat
-    libero bibendum.`,
-    `Aliquam erat volutpat. Nullam non mi nec ex fringilla eleifend. Praesent auctor
-    ligula at ex maximus, vitae pretium risus ultricies.`,
+    `Sebastián Cifuentes Aravena, conocido artísticamente como Sebastián Presley, 
+    es un talentoso cantante chileno de 22 años originario de Chillán. 
+    Desde temprana edad, mostró una profunda admiración por Elvis Presley, 
+    lo que lo llevó a perfeccionar su interpretación vocal y escénica del "Rey del Rock". 
+    Su dedicación y pasión por la música lo han posicionado como el doble oficial de Elvis en Chile.`,
+    `Sebastián ganó notoriedad nacional al convertirse en finalista del programa de talentos "Mi Nombre Es" de TVN en 2025, 
+    donde deslumbró al jurado y al público con su carisma 
+    y habilidad vocal al interpretar a Elvis Presley. 
+    A pesar de enfrentar desafíos personales y críticas que pusieron en duda su autenticidad como imitador, 
+    encontró fortaleza en el apoyo de su madre 
+    y perseveró en su sueño de rendir homenaje a uno de los más grandes íconos de la música mundial.`,
+    `Actualmente, Sebastián continúa desarrollando su carrera artística, realizando presentaciones en diversos eventos 
+    y compartiendo contenido relacionado con su tributo a Elvis Presley en sus redes sociales. 
+    Puedes seguir su trabajo en su cuenta de Instagram: @elvissebastianpresley.`,
+    `¿Quién fue Elvis Presley?
+    Elvis Aaron Presley (1935-1977) fue un cantante y actor estadounidense, 
+    ampliamente conocido como el "Rey del Rock and Roll". 
+    Nacido en Tupelo, Mississippi, y criado en Memphis, Tennessee, 
+    Elvis comenzó su carrera musical en 1954 con Sun Records. 
+    Su estilo único fusionó géneros como el country, el blues y el gospel, 
+    revolucionando la música popular y convirtiéndose en una figura icónica del siglo XX.`,
   ];
 
   return (
